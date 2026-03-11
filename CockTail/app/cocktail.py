1f67bedbd61c56e9a198a6beff9abe39fe12d2e0
Ryan <ryanleibbrand0@gmail.com>
2026-03-11 14:56:50 +0100

    added cocktail endproject for python advanced

---
+++ CockTail/app/cocktail.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template, url_for, redirect  
+import jinja2, requests  
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/results")
+def results():
+    query = request.args.get("query")
+    data = requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={query}")
+    data = data.json()
+    print(data["drinks"])
+    return render_template('results.html', data=data['drinks'])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
